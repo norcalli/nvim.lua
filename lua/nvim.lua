abd1e800bd950ff3f32489212543d0014f7aef00
Ashkan Kiani <ashkan.k.kiani@gmail.com>
2019-12-03 08:20:20 -0800

    Fix bugs.

--- lua/nvim.lua
+++ lua/nvim.lua
@@ -1,25 +1,34 @@
 -- Bring vim into local scope.
 local vim = vim
+local api = vim.api
+local inspect = vim.inspect
+
+local function extend(t, o)
+  for k, v in pairs(o) do
+    rawset(t, k, v)
+  end
+  return t
+end
 
 -- Equivalent to `echo vim.inspect(...)`
 local function nvim_print(...)
   if select("#", ...) == 1 then
-    vim.api.nvim_out_write(vim.inspect((...)))
+    api.nvim_out_write(inspect((...)))
   else
-    vim.api.nvim_out_write(vim.inspect {...})
+    api.nvim_out_write(inspect {...})
   end
-  vim.api.nvim_out_write("\n")
+  api.nvim_out_write("\n")
 end
 
 --- Equivalent to `echo` EX command
 local function nvim_echo(...)
   for i = 1, select("#", ...) do
     local part = select(i, ...)
-    vim.api.nvim_out_write(tostring(part))
+    api.nvim_out_write(tostring(part))
     -- vim.api.nvim_out_write("\n")
-    vim.api.nvim_out_write(" ")
+    api.nvim_out_write(" ")
   end
-  vim.api.nvim_out_write("\n")
+  api.nvim_out_write("\n")
 end
 
 local window_options = {
@@ -44,63 +53,22 @@ local window_options = {
      winfixwidth = true; winhighlight = true;         winhl = true;           wrap = true;
 }
 
--- `nvim.$method(...)` redirects to `nvim.api.nvim_$method(...)`
--- `nvim.fn.$method(...)` redirects to `vim.api.nvim_call_function($method, {...})`
--- TODO `nvim.ex.$command(...)` is approximately `:$command {...}.join(" ")`
--- `nvim.print(...)` is approximately `echo vim.inspect(...)`
--- `nvim.echo(...)` is approximately `echo table.concat({...}, '\n')`
--- Both methods cache the inital lookup in the metatable, but there is a small overhead regardless.
-local nvim = setmetatable({
-  print = nvim_print;
-  echo = nvim_echo;
-  fn = vim.fn or setmetatable({}, {
-    __index = function(t, k)
-      local f = function(...) return vim.api.nvim_call_function(k, {...}) end
-      rawset(t, k, f)
-      return f
+local function validate(conf)
+  assert(type(conf) == 'table')
+  local type_names = {
+    t='table', s='string', n='number', b='boolean', f='function', c='callable',
+    ['table']='table', ['string']='string', ['number']='number',
+    ['boolean']='boolean', ['function']='function', ['callable']='callable',
+    ['nil']='nil', ['thread']='thread', ['userdata']='userdata',
+  }
+  for k, v in pairs(conf) do
+    if not (v[3] and v[1] == nil) and type(v[1]) ~= type_names[v[2]] then
+      error(string.format("validation_failed: %q: expected %s, received %s", k, type_names[v[2]], type(v[1])))
     end
-  });
-  buf = setmetatable({
-  }, {
-    __index = function(t, k)
-      local f
-      if k == 'line' then
-        f = function()
-          local pos = vim.api.nvim_win_get_cursor(0)
-          return vim.api.nvim_buf_get_lines(0, pos[1]-1, pos[1], 'line')[1]
-        end
-      elseif k == 'nr' then
-        f = vim.api.nvim_get_current_buf
-      end
-      rawset(t, k, f)
-      return f
-    end
-  });
-  ex = setmetatable({}, {
-    __index = function(t, k)
-      local command = k:gsub("_$", "!")
-      local f = function(...)
-        return vim.api.nvim_command(table.concat(vim.tbl_flatten {command, ...}, " "))
-      end
-      rawset(t, k, f)
-      return f
-    end
-  });
-}, {
-  __index = function(self, k)
-    local mt = getmetatable(self)
-    local x = mt[k]
-    if x ~= nil then
-      return x
-    end
-    local f = vim.api['nvim_'..k]
-    mt[k] = f
-    return f
   end
-})
+  return true
+end
 
-local a = vim.api
-local validate = vim.validate
 local function make_meta_accessor(get, set, del)
   validate {
     get = {get, 'f'};
@@ -125,73 +93,125 @@ local function make_meta_accessor(get, set, del)
   end
   return setmetatable({}, mt)
 end
+
 local function pcall_ret(status, ...)
   if status then return ... end
 end
+
 local function nil_wrap(fn)
   return function(...)
     return pcall_ret(pcall(fn, ...))
   end
 end
-nvim.g = make_meta_accessor(nil_wrap(a.nvim_get_var), a.nvim_set_var, a.nvim_del_var)
-nvim.v = make_meta_accessor(nil_wrap(a.nvim_get_vvar), a.nvim_set_vvar)
-nvim.o = make_meta_accessor(a.nvim_get_option, a.nvim_set_option)
+
+local fn = setmetatable({}, {
+  __index = function(t, k)
+    local f = function(...) return api.nvim_call_function(k, {...}) end
+    rawset(t, k, f)
+    return f
+  end
+})
+
 local function getenv(k)
-  local v = vim.fn.getenv(k)
+  local v = fn.getenv(k)
   if v == vim.NIL then
     return nil
   end
   return v
 end
-nvim.env = vim.env or make_meta_accessor(getenv, vim.fn.setenv)
+
 local function new_win_accessor(winnr)
-  local function get(k)    return a.nvim_win_get_var(winnr, k) end
-  local function set(k, v) return a.nvim_win_set_var(winnr, k, v) end
-  local function del(k)    return a.nvim_win_del_var(winnr, k) end
+  local function get(k)    return api.nvim_win_get_var(winnr, k) end
+  local function set(k, v) return api.nvim_win_set_var(winnr, k, v) end
+  local function del(k)    return api.nvim_win_del_var(winnr, k) end
   return make_meta_accessor(nil_wrap(get), set, del)
 end
-nvim.w = new_win_accessor(0)
-getmetatable(nvim.w).__call = function(_, winnr)
-  return new_win_accessor(winnr)
-end
+
 local function new_buf_accessor(bufnr)
-  local function get(k)    return a.nvim_buf_get_var(bufnr, k) end
-  local function set(k, v) return a.nvim_buf_set_var(bufnr, k, v) end
-  local function del(k)    return a.nvim_buf_del_var(bufnr, k) end
+  local function get(k)    return api.nvim_buf_get_var(bufnr, k) end
+  local function set(k, v) return api.nvim_buf_set_var(bufnr, k, v) end
+  local function del(k)    return api.nvim_buf_del_var(bufnr, k) end
   return make_meta_accessor(nil_wrap(get), set, del)
 end
-nvim.b = new_buf_accessor(0)
-getmetatable(nvim.b).__call = function(_, bufnr)
-  return new_buf_accessor(bufnr)
-end
+
 local function new_buf_opt_accessor(bufnr)
   local function get(k)
     if window_options[k] then
-      return a.nvim_err_writeln(k.." is a window option, not a buffer option")
+      return api.nvim_err_writeln(k.." is a window option, not a buffer option")
     end
-    return a.nvim_buf_get_option(bufnr, k)
+    return api.nvim_buf_get_option(bufnr, k)
   end
   local function set(k, v)
     if window_options[k] then
-      return a.nvim_err_writeln(k.." is a window option, not a buffer option")
+      return api.nvim_err_writeln(k.." is a window option, not a buffer option")
     end
-    return a.nvim_buf_set_option(bufnr, k, v)
+    return api.nvim_buf_set_option(bufnr, k, v)
   end
   return make_meta_accessor(nil_wrap(get), set)
 end
-nvim.bo = vim.bo or new_buf_opt_accessor(0)
-getmetatable(nvim.bo).__call = function(_, bufnr)
-  return new_buf_opt_accessor(bufnr)
-end
+
 local function new_win_opt_accessor(winnr)
-  local function get(k)    return a.nvim_win_get_option(winnr, k) end
-  local function set(k, v) return a.nvim_win_set_option(winnr, k, v) end
+  local function get(k)    return api.nvim_win_get_option(winnr, k) end
+  local function set(k, v) return api.nvim_win_set_option(winnr, k, v) end
   return make_meta_accessor(nil_wrap(get), set)
 end
-nvim.wo = vim.wo or new_win_opt_accessor(0)
-getmetatable(nvim.wo).__call = function(_, winnr)
-  return new_win_opt_accessor(winnr)
-end
 
-return nvim
+-- `nvim.$method(...)` redirects to `nvim.api.nvim_$method(...)`
+-- `nvim.fn.$method(...)` redirects to `vim.api.nvim_call_function($method, {...})`
+-- TODO `nvim.ex.$command(...)` is approximately `:$command {...}.join(" ")`
+-- `nvim.print(...)` is approximately `echo vim.inspect(...)`
+-- `nvim.echo(...)` is approximately `echo table.concat({...}, '\n')`
+-- Both methods cache the inital lookup in the metatable, but there is api small overhead regardless.
+return setmetatable({
+  print = nvim_print;
+  echo = nvim_echo;
+  fn = rawget(vim, "fn") or fn;
+  validate = validate;
+  g = make_meta_accessor(nil_wrap(api.nvim_get_var), api.nvim_set_var, api.nvim_del_var);
+  v = make_meta_accessor(nil_wrap(api.nvim_get_vvar), api.nvim_set_vvar);
+  o = make_meta_accessor(api.nvim_get_option, api.nvim_set_option);
+  env = rawget(vim, "env") or make_meta_accessor(getenv, fn.setenv);
+  w = extend(new_win_accessor(0), {
+    __call = function(_, winnr)
+      return new_win_accessor(winnr)
+    end
+  });
+  wo = rawget(vim, "wo") or extend(new_win_opt_accessor(0), {
+    __call = function(_, winnr)
+      return new_win_opt_accessor(winnr)
+    end
+  });
+  b = extend(new_buf_accessor(0), {
+    __call = function(_, bufnr)
+      return new_buf_accessor(bufnr)
+    end
+  });
+  bo = rawget(vim, "bo") or extend(new_buf_opt_accessor(0), {
+    __call = function(_, bufnr)
+      return new_buf_opt_accessor(bufnr)
+    end
+  });
+  buf = {
+    line = api.nvim_get_current_line;
+    nr = api.nvim_get_current_buf;
+  };
+  ex = setmetatable({}, {
+    __index = function(t, k)
+      local command = k:gsub("_$", "!")
+      local f = function(...)
+        return api.nvim_command(table.concat(vim.tbl_flatten {command, ...}, " "))
+      end
+      rawset(t, k, f)
+      return f
+    end
+  });
+}, {
+  __index = function(t, k)
+    local f = api['nvim_'..k]
+    if f then
+      rawset(t, k, f)
+    end
+    return f
+  end
+})
 -- vim:et ts=2 sw=2
